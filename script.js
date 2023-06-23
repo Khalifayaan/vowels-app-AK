9beb604d6fbc36dc26ec7e32aea9f7d5fab456f2
Ayaan Khalif <ayaankhalif@Ayaans-MacBook-Air-4.local>
2023-06-23 21:02:03 +0300

    vowels-app-assignment-Done

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+
+const inputText = document.getElementById('input-text');
+const countBtn = document.getElementById('count-btn');
+const vowelCount = document.getElementById('vowel-count');
+
+
+function countVowels() {
+  const text = inputText.value.toLowerCase();
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  let count = 0;
+
+  for (let i = 0; i < text.length; i++) {
+    if (vowels.includes(text[i])) {
+      count++;
+    }
+  }
+
+  vowelCount.textContent = count;
+}
+
+countBtn.addEventListener('click', countVowels);
